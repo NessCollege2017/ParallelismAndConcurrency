2bef9ae0cda27d95f99a87a58781647eca77e287
NessCollege <nesscollege2017@gmail.com>
2017-05-19 12:55:46 +0300

    get and futuretask

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,28 +3,28 @@ package com.company;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         //consumer
         //shutdown
         ExecutorService threadPool = Executors.newFixedThreadPool(4);
-        for (int i = 0; i < 10; i++) {
-            threadPool.execute(() -> {
-                try {
-                    Thread.sleep(1000);
-                    threadPool.execute((Main::displayTime));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
 
+
+        Future<String> futureResult = threadPool.submit(() -> {
+            Thread.sleep(1000);
+            return "Surprise!";
+        });
+
+        System.out.println("Main");
+
+        System.out.println("Main is doing some work");
+
+        System.out.println("Main is now waiting for the futureResult");
+        System.out.println(futureResult.get());
+        System.out.println("Main is done waiting for the futureResult");
         threadPool.shutdown();
     }
 
