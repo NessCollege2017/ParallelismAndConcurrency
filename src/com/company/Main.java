8879771299756e1b2f5d7690a86315d97768b94a
NessCollege <nesscollege2017@gmail.com>
2017-05-19 12:26:01 +0300

    Schedualed ThreadPool

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,45 @@
 package com.company;
 
 
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) {
+        SheepHerd herd = new SheepHerd();
+        ExecutorService pool = Executors.newCachedThreadPool();
 
-        ExecutorService service = Executors.newSingleThreadExecutor();
 
-        service.execute(()-> System.out.println("Running the task..."));
-        System.out.println("Main");
-        service.execute(()-> System.out.println("Running the task..."));
-        System.out.println("Main");
-        service.execute(()-> System.out.println("Running the task..."));
-        System.out.println("Main");
-        service.execute(()-> System.out.println("Running the task..."));
-        System.out.println("Main");
+        ScheduledExecutorService sched = Executors.newScheduledThreadPool(1);
+        sched.scheduleAtFixedRate(()->{
+            System.out.println(LocalTime.now());
+        }, 1, 1, TimeUnit.SECONDS);
 
+        pool.execute(herd::addSheepAndCount);
+
+        ArrayList<String> data = new ArrayList<>();
+        data.add("A");
+        data.add("B");
+        data.add("C");
+        data.add("D");
+
+        data.forEach(Main::readData);
+
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        service.execute(Main::doStuff);
         service.shutdown();
+    }
+
+    public static void doStuff(){
+        System.out.println("Doing stuff.");
+    }
+    //void accept(T result)
+    public static void readData(String s){
+        System.out.println("Read " + s);
     }
 }
